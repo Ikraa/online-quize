fe4dd76f5d563f99e75fd12b87b534d301a00999
munnisaikra <munnisaikra750@gmail.com>
2022-12-28 13:58:16 +0600

    correction

--- src/Componant/NotFound/notFound.js
+++ src/Componant/NotFound/notFound.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="text-center">
-      <h1 className="text-6xl pt-10 pb-4 text-red-700 font-bold">404</h1>
+      <h1 className="text-6xl pt-10 pb-4 text-red-600 font-bold">404!!!</h1>
       <p className="text-black-400 text-3xl">Page not found</p>
       <p className="text-black-400 text-xl pt-4">
         The page you are looking for doesn't exist or another error occurred.
